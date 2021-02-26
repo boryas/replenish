d32e7a391bbde8d71b0d8d82a4afbe1f21da8d7c
Boris Burkov <boris@bur.io>
2021-02-25 16:09:55 -0800

    primitives for i64, names; binop

--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@ rustfmt
 define a simple env
 start bootstrapping parsing
 hack on path/env till you can run ls
+play with no-copy on &str instead of String
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 extern crate nom;
 use nom::{
   IResult,
-  character::complete::alphanumeric1,
+  branch::{alt},
+  character::complete::{alphanumeric1,digit1,one_of},
+  combinator::{map_res},
+  sequence::{tuple},
 };
 
-/* TODO: decide if you want to "do it live" while the input is still valid, or
- * if your String should be copied.
- */
+#[derive(Debug,PartialEq)]
+pub enum BinOp {
+  Add,
+  Sub,
+  Mul,
+  Div,
+  Exp,
+  Log
+}
+
 #[derive(Debug,PartialEq)]
 pub enum Expr {
-  Int(i32),
-  Iden(String)
+  Int64(i64),
+  Iden(String),
+  BinOp(BinOp, Box<Expr>, Box<Expr>)
 }
 
 fn identifier(input: &str) -> IResult<&str, Expr> {
@@ -19,9 +30,59 @@ fn identifier(input: &str) -> IResult<&str, Expr> {
   Ok((input, Expr::Iden(id.to_string())))
 }
 
+fn int64(input: &str) -> IResult<&str, Expr> {
+  let (input, i) = map_res(
+    digit1,
+    |ds: &str| i64::from_str_radix(&ds, 10))(input)?;
+
+  Ok((input, Expr::Int64(i)))
+}
+
+fn value(input: &str) -> IResult<&str, Expr> {
+  let (input, v) = alt((int64, identifier))(input)?;
+
+  Ok((input, v))
+}
+
+fn binop(input: &str) -> IResult<&str, Expr> {
+  let (input, (e1, op, e2)) = tuple((value, one_of("+-*/"), value))(input)?;
+  Ok((input, Expr::BinOp(
+        match op {
+          '+' => BinOp::Add,
+          '-' => BinOp::Sub,
+          '*' => BinOp::Mul,
+          '/' => BinOp::Div,
+          _ => unreachable!()
+        }, Box::new(e1), Box::new(e2))))
+}
+
 fn main() {}
 
 #[test]
 fn parse_id() {
   assert_eq!(identifier("foo"), Ok(("", Expr::Iden("foo".to_string()))));
+}
+
+#[test]
+fn parse_i64() {
+  assert_eq!(int64("42"), Ok(("", Expr::Int64(42))));
+}
+
+#[test]
+fn parse_val() {
+  assert_eq!(value("foo"), Ok(("", Expr::Iden("foo".to_string()))));
+  assert_eq!(value("42"), Ok(("", Expr::Int64(42))));
+}
+
+#[test]
+fn parse_binop() {
+  let res = binop("foo+42");
+  match res {
+    Ok(("", Expr::BinOp(op, e1, e2))) => {
+      assert_eq!(op, BinOp::Add);
+      assert_eq!(*e1, Expr::Iden("foo".to_string()));
+      assert_eq!(*e2, Expr::Int64(42));
+    }
+    _ => assert_eq!(true, false)
+  };
 }
